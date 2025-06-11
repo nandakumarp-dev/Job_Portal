be2c50d83a69f712fa1ea790e44112766eb3bc0d
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-06-11 15:06:38 +0000

    remapping apps

--- job_portal/home/urls.py
+++ job_portal/home/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
 
-    path('home/',views.HomeView.as_view(),name='home_page'),
+    path('',views.HomeView.as_view(),name='home_page'),
     path('about/',views.AboutView.as_view(),name='about_page'),
     path('contact/',views.ContactView.as_view(),name='contact_page'),
-    # path('postjob/',views.PostJobView.as_view(),name='post_job_page'),
     path('userselection/',views.UserSelectionView.as_view(),name='user_selection_page'),
     path('dashboard/employer/', views.EmployerDashboardView.as_view(), name='employer_dashboard'),
     path('dashboard/employee/', views.EmployeeDashboardView.as_view(), name='jobseeker_dashboard'),
